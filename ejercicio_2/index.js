c48786eca319aedcbc9e463e35fe6ba10cf806ab
piamisa <54381926+piamisa@users.noreply.github.com>
2022-08-18 16:47:31 -0300

    Add files via upload

---
+++ ejercicio_2/index.js
@@ -0,0 +1,11 @@
+const fechaNacimiento = new Date(1998, 4, 7)
+
+const libroFavorito = {
+    titulo: "Historia de Cronopios y de Famas",
+    autor: "Julio Cortázar",
+    fecha: 1962,
+    url: "https://es.wikipedia.org/wiki/Historias_de_cronopios_y_de_famas"
+}
+const lista = ["Pia", 24, true, fechaNacimiento, libroFavorito]
+
+console.log(lista)
